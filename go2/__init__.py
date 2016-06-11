428b4f5826feeefcd844ff52052316023cd7e954
Frederick Gnodtke <frederick@gnodtke.net>
2016-06-12 00:13:04 +0200

    Fixed board and added unittests for it.

---
+++ go2/__init__.py
@@ -0,0 +1,7 @@
+from go2.gamelogic.game import Game
+from go2.gamelogic.board import Color
+
+game = Game(9)
+
+game.place_token((3, 3), Color.BLACK)
+
--- go2/gamelogic/game.py
+++ go2/gamelogic/game.py
@@ -1,4 +1,4 @@
-from board import Board, Color
+from go2.gamelogic.board import Board, Color
 from copy import copy
 class Game:
     def __init__(self, size):
